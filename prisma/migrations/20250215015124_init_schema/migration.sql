66ba23a845f5488adb1becdcf54b910481aada95
sohelhussain <sohelhussaing@gmail.com>
2025-02-15 07:28:48 +0530

    feat: ADDING A PRISMA

---
+++ prisma/migrations/20250215015124_init_schema/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Email" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Email_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Phone" (
+    "id" SERIAL NOT NULL,
+    "phone" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Phone_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Email" ADD CONSTRAINT "Email_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Phone" ADD CONSTRAINT "Phone_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import {PrismaClient} from '@prisma/client';
 
 const app = express();
+const client = new PrismaClient();
 
-app.get('/', (req, res) => {
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+
+app.post("/identify", async (req: Request, res: Response) => {
+  const {email, phone} = req.body;
+  const response = await client.user.update({
+    where: {
+      id: 1
+    },
+    data: {
+      email: email,
+      phone: phone
+    }
+  })
+
+  res.send(response);
+});
+
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
